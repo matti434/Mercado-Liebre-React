4a79183bf5ae75a66bdb26e5cd56b1dd2d67bdef
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-10-27 19:16:54 -0300

    feat: add CarouselHome in new view folder

--- src/Componentes/pages/Home.jsx
+++ src/Componentes/pages/Home.jsx
@@ -1,14 +1,24 @@
 import { Container } from "react-bootstrap";
-
-
+import CarouselHome from '../view/CarouselHome';
 const Home = () => {
   return (
     <Container className="mt-4">
-    <div className="text-center mb-5">
-       <h1>Bienvenido a MercadoLibre</h1>
-       <p className="lead">Encuentra los mejores productos al mejor precio</p>
-       
-    </div>
+  
+      <section className="text-center mt-5 mb-5">
+        <h1>Bienvenido a MercadoLibre</h1>
+        <p className="lead">Encuentra los mejores productos al mejor precio</p>      
+      </section>
+      
+
+      <section className="border-top pt-5 mt-5">
+        <div className="text-center">
+          <h2>Productos Destacados</h2>
+        </div>
+
+        <div> 
+          <CarouselHome />           
+        </div>
+      </section>
     </Container>
   )
 }
